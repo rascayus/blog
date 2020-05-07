5e515a16a10a3dc8e0b12977139858c25e985561
Rascayus <juandialonso@gmail.com>
2020-05-07 11:58:25 +0200

    Añadido usuario

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,13 +13,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 29)->create();
+        factory(App\User::class, 28)->create();
 
 
         App\User::create([
         'name'              => 'Juan Diego',
         'email'             => 'juandialonso@gmail.com',
-        'password'          => '$2y$10$Kt4nGR5QAnrND6zv1aZvleYbgj3jp2o9gK3uXFrXfxdQ9goAxy5Zq',
+        'password'          =>  bcrypt('123456'),
+        'remember_token'    => str_random(10),
+        ]);
+        App\User::create([
+        'name'              => 'Javier Llorens',
+        'email'             => 'llorens_jav@gva.es',
+        'password'          =>  bcrypt('severo'),
         'remember_token'    => str_random(10),
         ]);
       
